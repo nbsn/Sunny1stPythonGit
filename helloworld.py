b0584ed65840b7ec8ace6bb4397973dc10f81be2
sunny_zhang <sunnyzhang2@gmail.com>
2020-02-20 00:01:09 +0800

    Git update test from PyCharm

--- helloworld.py
+++ helloworld.py
@@ -2,4 +2,5 @@ import datetime
 print('hello world')
 print('hello sunny')
 print('welcome to vip')
-print('Git update test')
+print('Git update test1')
+print('git update test2')
--- helloworld2.py
+++ helloworld2.py
@@ -1,3 +1,4 @@
 print('hello world2')
 print('hello sunny3')
 print('welcome to vip3')
+print('git update test2')
